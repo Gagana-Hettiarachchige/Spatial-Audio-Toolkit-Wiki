af51736daeb85335c37defcb068678f7bc969a96
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2025-04-14 12:21:35 -0400

    Added change to remove uncessasary class definition.

--- sat-wiki/app/layout.tsx
+++ sat-wiki/app/layout.tsx
@@ -32,8 +32,8 @@ export default function RootLayout({
         <nav className="navbar">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
+              {/* <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
                   <span className="absolute -inset-0.5"></span>
                   <span className="sr-only">Open main menu</span>
                   <svg className="block size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
@@ -42,8 +42,8 @@ export default function RootLayout({
                   <svg className="hidden size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                   </svg>
-                </button> */}
-              </div>
+                </button>
+              </div> */}
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex shrink-0 items-center">
                 </div>
@@ -64,7 +64,7 @@ export default function RootLayout({
           {/* Mobile menu */}
           <div className="sm:hidden" id="mobile-menu">
             <div className="space-y-1 px-2 pt-2 pb-3">
-              <a href="/" className="creation-story block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
+              <a href="/" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
               <a href="/creation-story" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Creation Story</a>
               <a href="/app-help" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">App Help</a>
               {/* <a href="/about-team" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About Team</a> */}
